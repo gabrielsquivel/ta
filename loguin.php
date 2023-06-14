7f180ba610de782e39ff741e755bee5fe3339c60
gabrielsquivel <119131795+gabrielsquivel@users.noreply.github.com>
2023-06-14 17:12:03 -0600

    loguin

---
+++ loguin.php
@@ -0,0 +1,30 @@
+
+<form Action="27php echo $-Server [PHP_self']; ?> method="post" <input type="text" name="Usuario" placeholder="Usuario")
+
+<input type="password" name="pass" placeholder="Password") <input type="submit" value=" Ingresar"" name="btn">
+
+<1 form)
+
+<?php
+
+if ($usuario D = = $user and $ pasum == $pass) (
+
+echo "Bienvenido". $user;
+
+$ Usuar6D="Esquivel"; password=" 54721
+
+elses
+
+$user = $_POST['Usuario'];
+
+echo "Datos no Concatos"";
+
+$ pas = $_POST['pass'];
+
+{Contadott,
+
+if($ver" and $pass =="" ""){ echo "los datos estan Uucios";
+
+18 (Contador >3){ echo Adios":
+
+else?
